3215763bf2cffdf13c9fd30910910baf840693b3
Mohamed Yassir Khalid <git@atkyms.com>
2020-05-05 06:36:35 +0000

    changed sameSite cookie to 'none'

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -127,7 +127,9 @@ adminRouter.post('/admin/login', async (req, res) => {
 			expires: new Date(Date.now() + 6 * 60 * 60 * 1000),
 
 			// sameSite
-			sameSite: true,
+//			sameSite: false,
+			sameSite: 'none',
+
 		});
 		//		res.set('Authorization', 'Bearer ' + loginToken);
 
