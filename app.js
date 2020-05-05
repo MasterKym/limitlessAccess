3ce0e88a57c566de41a51ef38d72474a84f5b54e
Mohamed Yassir Khalid <git@atkyms.com>
2020-05-05 07:14:12 +0000

    changed accepted origin on received requests

--- app.js
+++ app.js
@@ -57,8 +57,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Set Up CORS SETTINGs
+
 app.use('', (req, res, next) => {
-	res.header('Access-Control-Allow-Origin', req.headers.origin); // update to match the domain you will make the request from
+	const frontendDomain = `https://limitlessaccess.netlify.app`;
+	const origin =
+		process.env.NODE_ENV === 'dev' ? req.headers.origin : frontendDomain;
+
+	res.header('Access-Control-Allow-Origin', frontendDomain); // update to match the domain you will make the request from
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
 
 	res.header('Access-Control-Allow-Credentials', true);
